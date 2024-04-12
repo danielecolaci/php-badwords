3bc30a779f9cc485c197ae49b2c866772afdaf31
danielecolaci <danielecolaci@outlook.com>
2024-04-12 18:56:01 +0200

    Initial Commit - Add php files

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+
+$name = 'Hello'
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+    <h1><?php echo $name ?></h1>
+</body>
+</html>
