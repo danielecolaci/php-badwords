da0c9e34ba1a1112d619d7b8560b19d88a5dfc7e
danielecolaci <danielecolaci@outlook.com>
2024-04-12 19:26:56 +0200

    Add text length to the DOM

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
 
-$name = 'Hello'
+$name = 'Hello';
+var_dump($_GET);
 
 ?>
 
@@ -10,7 +11,7 @@ $name = 'Hello'
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Badwords</title>
+    <title>PHP Badwords Input</title>
 
     <style>
         body{
--- script.php
+++ script.php
@@ -3,4 +3,38 @@
 $text = $_GET['text'];
 $ban = $_GET['ban'];
 
+var_dump($_GET);
+
+$banned_word = '*****';
+
+$text_length = strlen($text);
+
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords Results</title>
+
+    <style>
+        body{
+            text-align: center;
+        }
+
+        div{
+            border: 1px solid black;
+            border-radius: 4px;
+        }
+    </style>
+</head>
+<body>
+    <div>    
+    <h2>Text</h2>
+    <p><?php echo $text ?></p>
+    <h4>Length: <i><?php echo $text_length ?> characters.</i></h4>
+    </div>
+</body>
+</html>
